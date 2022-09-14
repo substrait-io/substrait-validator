664e062c67f520451ffe2f0a4165d1a7121deb54
Matthijs Brobbel <m1brobbel@gmail.com>
2022-09-14 12:21:32 +0200

    fix: summary improvements for nullable join predicates

--- rs/src/parse/relations/join.rs
+++ rs/src/parse/relations/join.rs
@@ -38,10 +38,9 @@ pub fn parse_join_rel(x: &substrait::JoinRel, y: &mut context::Context) -> diagn
     }
 
     // Parse join expression.
-    let join_expression =
-        proto_boxed_required_field!(x, y, expression, expressions::parse_predicate)
-            .1
-            .unwrap_or_default();
+    let (join_expression_node, opt_join_expression) =
+        proto_boxed_required_field!(x, y, expression, expressions::parse_predicate);
+    let join_expression = opt_join_expression.unwrap_or_default();
 
     // If the join expression is a literal it must be True.
     if let expressions::Expression::Literal(ref literal) = join_expression {
@@ -101,7 +100,7 @@ pub fn parse_join_rel(x: &substrait::JoinRel, y: &mut context::Context) -> diagn
 
     // Handle optional post-join filter.
     let filter_expression =
-        proto_boxed_field!(x, y, post_join_filter, expressions::parse_predicate).1;
+        proto_boxed_field!(x, y, post_join_filter, expressions::parse_predicate);
 
     // Describe the relation.
     let prefix = match (join_type, x.post_join_filter.is_some()) {
@@ -123,59 +122,81 @@ pub fn parse_join_rel(x: &substrait::JoinRel, y: &mut context::Context) -> diagn
     };
     describe!(y, Relation, "{prefix} join by {join_expression}");
     summary!(y, "{prefix} join by {join_expression:#}.");
-    y.push_summary(comment::Comment::new().nl().plain(match join_type {
-        JoinType::Unspecified => "",
-        JoinType::Inner => concat!(
-            " Returns rows combining the row from the left and right ",
-            "input for each pair where the join expression yields true.",
-        ),
-        JoinType::Outer => concat!(
-            " Returns rows combining the row from the left and right ",
-            "input for each pair where the join expression yields true. ",
-            "If the join expression never yields true for any left or ",
-            "right row, this returns a row anyway, with the fields ",
-            "corresponding to the other input set to null.",
-        ),
-        JoinType::Left => concat!(
-            " Returns rows combining the row from the left and right ",
-            "input for each pair where the join expression yields true. ",
-            "If the join expression never yields true for a row from the ",
-            "left, this returns a row anyway, with the fields corresponding ",
-            "to the right input set to null.",
-        ),
-        JoinType::Right => concat!(
-            " Returns rows combining the row from the left and right ",
-            "input for each pair where the join expression yields true. ",
-            "If the join expression never yields true for a row from the ",
-            "right, this returns a row anyway, with the fields corresponding ",
-            "to the left input set to null.",
-        ),
-        JoinType::Semi => concat!(
-            " Filters rows from the left input, propagating a row only if ",
-            "the join expression yields true for that row combined with ",
-            "any row from the right input.",
-        ),
-        JoinType::Anti => concat!(
-            " Filters rows from the left input, propagating a row only if ",
-            "the join expression does not yield true for that row combined ",
-            "with any row from the right input.",
-        ),
-        JoinType::Single => concat!(
-            " Returns a row for each row from the left input, concatenating ",
-            "it with the row from the right input for which the join ",
-            "expression yields true. If the expression never yields true for ",
-            "a left input, the fields corresponding to the right input are ",
-            "set to null. If the expression yields true for a left row and ",
-            "multiple right rows, this may return the first pair encountered ",
-            "or throw an error."
-        ),
-    }));
-    if let Some(filter_expression) = filter_expression {
-        y.push_summary(
-            comment::Comment::new()
-                .nl()
-                .plain(format!("The result is filtered by {filter_expression:#}.")),
-        );
+    let nullable = if join_expression_node.data_type().nullable() {
+        "false or null"
+    } else {
+        "false"
+    };
+    y.push_summary(
+        comment::Comment::new().nl().plain(match join_type {
+            JoinType::Unspecified => "".to_string(),
+            JoinType::Inner => format!(
+                " 
+Returns rows combining the row from the left and right
+input for each pair where the join expression yields true.
+Discarding rows where the join expression yields {}.",
+                nullable
+            ),
+            JoinType::Outer => format!(
+                " 
+Returns rows combining the row from the left and right
+input for each pair where the join expression yields true.
+Discarding rows where the join expression yields {}.
+If the join expression never yields true for any left or
+right row, this returns a row anyway, with the fields
+corresponding to the other input set to null.",
+                nullable
+            ),
+            JoinType::Left => format!(
+                " 
+Returns rows combining the row from the left and right
+input for each pair where the join expression yields true.
+Discarding rows where the join expression yields {}.
+If the join expression never yields true for a row from the
+left, this returns a row anyway, with the fields corresponding
+to the right input set to null.",
+                nullable
+            ),
+            JoinType::Right => format!(
+                " 
+Returns rows combining the row from the left and right
+input for each pair where the join expression yields true.
+Discarding rows where the join expression yields {}.
+If the join expression never yields true for a row from the
+right, this returns a row anyway, with the fields corresponding
+to the left input set to null.",
+                nullable
+            ),
+            JoinType::Semi => " 
+Filters rows from the left input, propagating a row only if
+the join expression yields true for that row combined with
+any row from the right input."
+                .to_string(),
+            JoinType::Anti => " 
+Filters rows from the left input, propagating a row only if
+the join expression does not yield true for that row combined
+with any row from the right input."
+                .to_string(),
+            JoinType::Single => " 
+Returns a row for each row from the left input, concatenating
+it with the row from the right input for which the join
+expression yields true. If the expression never yields true for
+a left input, the fields corresponding to the right input are
+set to null. If the expression yields true for a left row and
+multiple right rows, this may return the first pair encountered
+or throw an error."
+                .to_string(),
+        }),
+    );
+
+    if let (Some(node), Some(filter_expression)) = filter_expression {
+        let nullable = node.data_type().nullable();
+        y.push_summary(comment::Comment::new().nl().plain(format!(
+            "
+The result is filtered by the expression {filter_expression:#}, 
+discarding all rows for which the filter expression yields {}.",
+            if nullable { "false or null" } else { "false" }
+        )));
     }
 
     // Handle the common field.
