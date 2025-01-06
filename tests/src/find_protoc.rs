ea04f06f08007bef62cf8cf8fdb98e9b949b0c5f
Wendell Smith <wendell.smith@datadoghq.com>
2025-01-06 17:09:29 -0500

    build(deps): also add protoc feature to py, c, and tests crates

--- tests/src/find_protoc.rs
+++ tests/src/find_protoc.rs
@@ -3,6 +3,20 @@
 //! Prost has some magic for finding the path to protoc, so let's use that in
 //! the Python code as well...
 
+use std::path::PathBuf;
+
 fn main() {
-    println!("{}", prost_build::protoc_from_env().display());
+    let protoc_path: PathBuf;
+    #[cfg(feature = "protoc")]
+    {
+        // Use vendored protobuf compiler if requested.
+        protoc_path = protobuf_src::protoc();
+        println!("cargo:warning=Using vendored protoc: {:?}", protoc_path);
+    }
+    #[cfg(not(feature = "protoc"))]
+    {
+        protoc_path = prost_build::protoc_from_env();
+    }
+
+    println!("{}", protoc_path.display());
 }
